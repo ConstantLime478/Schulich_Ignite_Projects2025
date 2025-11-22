ec1712b549308297fe112e2a2645e11a2f0bdbb7
ConstantLime478 <hayyanlime478@gmail.com>
2025-11-22 15:14:56 -0700

    Added my own sprite

--- Flare-Session6/moving_sprite.py
+++ Flare-Session6/moving_sprite.py
@@ -11,11 +11,10 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 FRAME_RATE = 60
-
-#  Movement and physics constants
-PLAYER_SPEED = 5 # Speed of the player in pixels per frame
-GRAVITY = 1  # Gravity effect in pixels per frame
-JUMP_STRENGTH = 15  # Initial jump velocity in pixels per frame
+JUMP_VELOCITY = -15
+y_speed = 5
+x_speed = 5
+GRAVITY = 9.8
 
 # Useful colors 
 BLACK = (0, 0, 0)
@@ -27,19 +26,18 @@ screen.set_alpha(0)  # Make alpha bits transparent
 clock = pygame.time.Clock()
 
 # Importing Important Classes
-from sprite_updated import Player
+from sprite import Player
 from platform2 import Platform
 
 # Creating our spirte and platform objects
 main_c = Player() # Our main character
-plat1 = Platform(50, 500, 300, 350/4)  # Platform 1
-plat2 = Platform(425, 600, 200, 250/4) # Platform 2
-plat3 = Platform(700, 550, 150, 200/4) # Platform 3
-plats = [plat1, plat2, plat3]
+plat1 = Platform(50, 500, 350, 350/4)  # Platform 1
+plat2 = Platform(425, 600, 250, 250/4) # Platform 2
+plat3 = Platform(700, 450, 200, 200/4) # Platform 3
 
 # Making the background
 # Taken from https://www.reddit.com/r/PixelArt/comments/j7751a/mt_fuji_study/
-background = pygame.image.load(os.path.join("Flare-Session5" , "assets", "background.png")).convert_alpha()
+background = pygame.image.load(os.path.join("Flare-Session4" , "assets", "background.png")).convert_alpha()
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 screen_rect = screen.get_rect()
 
@@ -63,60 +61,55 @@ while True:
         if event.type == pygame.QUIT:  # When user clicks the 'x' on the window, close our game
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            # JUMP
-            if event.key == pygame.K_UP and main_c.on_ground:
-                    main_c.velocity_y = -JUMP_STRENGTH
-                    main_c.on_ground = False   
 
-    # LEFT/RIGHT MOVEMENT
-    keys = pygame.key.get_pressed() # Stroing current key presses
-    dx = 0 # Change in x position
-    if keys[pygame.K_LEFT]:
-        dx -= PLAYER_SPEED
+    # Keyboard events
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_UP]:
+        
+        main_c.move(0, -y_speed) # Move main character up by 5 pixels
+        main_c.moving = True # Showing that he is moving
+    
+    elif keys_pressed[pygame.K_SPACE]:
+        y_speed = JUMP_VELOCITY
+        main_c.move(0, -y_speed) # Make the main character jump with a set velocity
+        main_c.moving = True # Showing that he is moving
+        y_speed += GRAVITY # Simulate gravity effect by increasing downward speed
+        main_c.rect.y += y_speed   
+        
+    elif keys_pressed[pygame.K_LEFT]:
+        
+        main_c.move(-x_speed, 0) # Move main character left by 5 pixels
         main_c.direction = "left" # Change sprite image direction
         main_c.frame_timer += 1 # Frame timer for animation
-    if keys[pygame.K_RIGHT]:
-        dx += PLAYER_SPEED
+        main_c.moving = True # Showing that he is moving
+        
+    elif keys_pressed[pygame.K_RIGHT]:
+        
+        main_c.move(x_speed, 0) # Move main character right by 5 pixels
         main_c.direction = "right" # Change sprite image direction
         main_c.frame_timer += 1 # Frame timer for animation
+        main_c.moving = True # Showing that he is moving
+        
+    elif keys_pressed[pygame.K_DOWN]:
+        
+        main_c.move(0, y_speed) # Move main character down by 5 pixels
+        main_c.moving = True # Showing that he is moving
+        
+    else:
+        main_c.moving = False # Showing that he is not moving
+        main_c.frame_timer = 0 # Reset frame timer when not moving
+        
+    # Mouse events
+    mouse_pos = pygame.mouse.get_pos()  # Get position of mouse as a tuple representing the (x, y) coordinate
+   
 
-    # GRAVITY
-    main_c.velocity_y += GRAVITY
-    dy = main_c.velocity_y # vertical movement
-
-    # Move on x-axis
-    main_c.rect.x += dx 
-
-    # Collision detection on x-axis
-    for plat in plats:
-        if pygame.sprite.collide_rect(main_c, plat):
-            if dx > 0:
-                main_c.rect.right = plat.rect.left  # hit platform from left
-            elif dx < 0:
-                main_c.rect.left = plat.rect.right  # hit platform from right
-
+    mouse_buttons = pygame.mouse.get_pressed()
+    if mouse_buttons[0]:  # If left mouse pressed
+        
+        main_c.x = mouse_pos[0] # Change x pos to mouse x pos
+        
+        main_c.y = mouse_pos[1] # Change y pos to mouse y pos
     
-    # move along y-axis
-    main_c.rect.y += dy
-
-    main_c.on_ground = False # reset on_ground before checking collisions
-
-    # Collision detection on y-axis
-    for plat in plats:
-        if pygame.sprite.collide_rect(main_c, plat):
-            # Check if falling
-            if main_c.velocity_y > 0 and main_c.rect.bottom >= plat.rect.top:
-                main_c.rect.bottom = plat.rect.top
-                main_c.on_ground = True
-                main_c.velocity_y = 0
-            # Check upward collision
-            elif main_c.velocity_y < 0 and main_c.rect.top <= plat.rect.bottom:
-                main_c.rect.top = plat.rect.bottom
-                main_c.velocity_y = 0
-
-    # update the moving value
-    main_c.moving = dx != 0
 
 
     """
--- Flare-Session6/sprite.py
+++ Flare-Session6/sprite.py
@@ -5,24 +5,19 @@ import os
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.falling = True
-        self.on_ground = False  # Start false
-        self.velocity_y = 0
         # Load all player images for animation
         # CHANGE THIS BASED ON YOUR COMPUTER'S FILE PATH STRUCTURE (READ THE README)
-        self.image_location = [os.path.join("Flare-Session5" ,"assets", "player1.png"), 
-                          os.path.join("Flare-Session5" ,"assets", "player2.png"), 
-                          os.path.join("Flare-Session5" ,"assets", "player3.png"), 
-                          os.path.join("Flare-Session5" ,"assets", "player4.png")] 
+        self.image_location = [os.path.join("Flare-Session4" ,"assets", "player1.png"), 
+                          os.path.join("Flare-Session4" ,"assets", "player2.png"), 
+                          os.path.join("Flare-Session4" ,"assets", "player3.png"), 
+                          os.path.join("Flare-Session4" ,"assets", "player4.png")] 
         
         # Load images into a list
-        self.frames = [pygame.transform.scale(pygame.image.load(self.image_location[0]).convert_alpha(), (75, 100)),
-                       pygame.transform.scale(pygame.image.load(self.image_location[1]).convert_alpha(), (75, 100)),
-                       pygame.transform.scale(pygame.image.load(self.image_location[2]).convert_alpha(), (75, 100)),
-                       pygame.transform.scale(pygame.image.load(self.image_location[3]).convert_alpha(), (75, 100))]
+        self.frames = [pygame.image.load(self.image_location[0]).convert_alpha(),
+                       pygame.image.load(self.image_location[1]).convert_alpha(),
+                       pygame.image.load(self.image_location[2]).convert_alpha(),
+                       pygame.image.load(self.image_location[3]).convert_alpha()]
 
-    
-        
         # Animation variables
         self.frame_index = 0 # Current frame index
         self.image = self.frames[self.frame_index] # Current image
@@ -31,6 +26,7 @@ class Player(pygame.sprite.Sprite):
         self.frame_timer = 0 # Frame timer
         self.direction = "right" # Player direction variable
         self.moving = False # Player moving variable
+        self.y_speed
 
         # Set the x and y values of the image
         self.rect.x = 100
@@ -71,7 +67,5 @@ class Player(pygame.sprite.Sprite):
     def move(self, x_change, y_change):
         self.rect.x += x_change # Move in the x direction
         self.rect.y += y_change # Move in the y direction
-        
-
 
         
